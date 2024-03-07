42499e8e26afb0ed7d5e4b8e8f216c0da3a3aa2f
regardue <147426303+regardue@users.noreply.github.com>
2024-03-07 22:38:03 +0200

    s

--- LoginPage.js
+++ LoginPage.js
@@ -9,12 +9,11 @@ function login() {
     (x) => x.email == email.value && x.password == password.value
   );
   console.log(user);
-    if (user.length > 0) {
-      // console.log("Login Succesful!");
-      alert("Login succesful")
-      window.location.href = "HomePage.html";
-    }
-    else{
-      alert("Invalid email or password!")
-    }
+  if (user.length > 0) {
+    // console.log("Login Succesful!");
+    alert("Login succesful");
+    window.location.href = "HomePage.html";
+  } else {
+    alert("Invalid email or password!");
+  }
 }
